405ff539f6509ef5ead19a8ed7bcd7e288b642ac
DavidKopecky <karatedava@seznam.cz>
2025-10-08 09:13:56 +0200

    release

---
+++ src/config.py
@@ -0,0 +1,14 @@
+"""
+DEFINITION OF GLOBAL PARAMETERS
+"""
+
+from pathlib import Path
+
+DATA_PATH = Path('./data')
+
+CONFIG_FOLDER = Path('./configs')
+DEFAULT_CONFIG_FILE = 'config_safe.json'
+OUTPUT_DIR = Path('./data')
+DEFAULT_SIMULATOR = 'GM' # YP is the other valid option
+DEFAULT_CTIME = 100
+DEFAULT_LB = 1300
